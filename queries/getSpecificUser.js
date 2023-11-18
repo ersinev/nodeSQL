0ef7f9e58b8d7ae727ecb4dc5745413235d3fe78
ersinev <ersinevci@hotmail.com>
2023-11-18 20:36:36 +0100

    updated

---
+++ queries/getSpecificUser.js
@@ -0,0 +1,8 @@
+// const getUserById = (userId) => {
+//     return {
+//       sql: 'SELECT * FROM nodesql.user WHERE id = ?',
+//       values: [userId],
+//     };
+//   };
+  
+//   module.exports = getUserById;
