a8319a260fd87b23dd0ed400ec9c17e6096bd793
ersinev <ersinevci@hotmail.com>
2023-11-20 00:03:33 +0100

    updated

--- app.js
+++ app.js
@@ -1,67 +1,65 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const mysql= require('mysql2')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 require('dotenv').config();
-const port = process.env.PORT ||5000
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-const userTableCreation = require('./table_creations/userTableCreation')
-const userDataInsertion = require('./data_insertion/userDataInsertion')
+
+const userTableCreation = require('./table_creations/userTableCreation');
+const userDataInsertion = require('./data_insertion/userDataInsertion');
 const getUserById = require('./queries/getSpecificUser');
 const productsTableCreation = require('./table_creations/productsTableCreation');
-const productsDataInsertion= require('./data_insertion/productsDataInsertion')
+const productsDataInsertion = require('./data_insertion/productsDataInsertion');
+
 const connection = mysql.createConnection({
-    host : "127.0.0.1",
+    host: "127.0.0.1",
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: "nodesql"
-})
+});
 
-connection.connect((err)=>{
-    if(err){
-        console.error('Error conecting to database:', err)
-        return
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
     }
-    console.log("Connected to database")
-})
-//TABLE CREATION
-connection.query(userTableCreation, (error, results, fields)=>{
-    if(error){
-        console.error('Error executing table creation query:', error)
-    }else{
-        console.log('User table created successfully')
-    }
-})
+    console.log("Connected to database");
+});
 
-connection.query(productsTableCreation, (error, result)=>{
+// TABLE CREATION
+connection.query(userTableCreation, (error, results, fields) => {
     if (error) {
-        console.error('Failed to create products table')
-        
-    }else{
-        console.log('products table created succesfully')
+        console.error('Error executing table creation query:', error);
+    } else {
+        console.log('User table created successfully');
     }
+});
 
-})
+connection.query(productsTableCreation, (error, result) => {
+    if (error) {
+        console.error('Failed to create products table');
+    } else {
+        console.log('Products table created successfully');
+    }
+});
 
-//INSERTING DATA
-app.post('/adduser', (req,res)=>{
-    const userData = req.body
-    
-    connection.query(userDataInsertion(userData),(insertError, insertResults)=>{
-        if(insertError){
-            console.error('Error inserting data', insertError)
-            res.status(500).send('Error inserting data')
-            return
-        }else{
-            console.log('Data inserted successfully')
-            res.send('User added successfully')
+// INSERTING DATA
+app.post('/adduser', (req, res) => {
+    const userData = req.body;
+
+    connection.query(userDataInsertion(userData), (insertError, insertResults) => {
+        if (insertError) {
+            console.error('Error inserting data', insertError);
+            res.status(500).send('Error inserting data');
+            return;
+        } else {
+            console.log('Data inserted successfully');
+            res.send('User added successfully');
         }
-    })
-  
-
-})
-
+    });
+});
 
 app.post('/addproduct', (req, res) => {
     const productData = req.body;
@@ -76,34 +74,33 @@ app.post('/addproduct', (req, res) => {
     });
 });
 
+// GET USER BY ID
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
 
-// app.get('/user/:id', (req, res) => {
-//     const userId = req.params.id;
-  
-//     // Use the getUserById function to create the SQL query string
-//     const query = getUserById(userId);
-  
-//     connection.query(query.sql, query.values, (err, result) => {
-//       if (err) {
-//         console.error('Error querying database:', err);
-//         res.status(500).send('Internal Server Error');
-//       } else {
-//         if (result.length > 0) {
-//           console.log(`User with ID ${userId}:`, result);
-//           res.status(200).json(result);
-//         } else {
-//           console.log(`User with ID ${userId} not found`);
-//           res.status(404).send('User not found');
-//         }
-//       }
-//     });
-//   });
+    // Use the getUserById function to create the SQL query string
+    const query = getUserById(userId);
 
-app.listen(port,()=>{
-    console.log(`server is running on ${port}`)
-})
+    connection.query(query.sql, query.values, (err, result) => {
+        if (err) {
+            console.error('Error querying database:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            if (result.length > 0) {
+                console.log(`User with ID ${userId}:`, result);
+                res.status(200).json(result);
+            } else {
+                console.log(`User with ID ${userId} not found`);
+                res.status(404).send('User not found');
+            }
+        }
+    });
+});
 
-app.get('/',(req,res)=>{
-    res.send('homepage')
-})
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
 
+app.get('/', (req, res) => {
+    res.send('Homepage');
+});
